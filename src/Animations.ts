65428118390bba6c508ae508eb5b69b97c4d13ee
Slash <slash@slashware.net>
2024-07-06 15:18:06 -0500

    Set anchor points for right hand

--- src/Animations.ts
+++ src/Animations.ts
@@ -9,88 +9,88 @@ const mikuAnimation = new Animation({
 		{
 			eyeX: 220,
 			eyeY: 176,
-			rightHandX: 220,
-			rightHandY: 176,
+			rightHandX: 301,
+			rightHandY: 240,
 			leftHandX: 158,
 			leftHandY: 258
 		},
 		{
 			eyeX: 220,
 			eyeY: 181,
-			rightHandX: 220,
-			rightHandY: 176,
+			rightHandX: 300,
+			rightHandY: 243,
 			leftHandX: 160,
 			leftHandY: 256
 		},
 		{
 			eyeX: 227,
 			eyeY: 179,
-			rightHandX: 220,
-			rightHandY: 176,
+			rightHandX: 307,
+			rightHandY: 241,
 			leftHandX: 160,
 			leftHandY: 258
 		},
 		{
 			eyeX: 231,
 			eyeY: 177,
-			rightHandX: 220,
-			rightHandY: 176,
+			rightHandX: 310,
+			rightHandY: 240,
 			leftHandX: 166,
 			leftHandY: 257
 		},
 		{
 			eyeX: 233,
 			eyeY: 174,
-			rightHandX: 220,
-			rightHandY: 176,
+			rightHandX: 311,
+			rightHandY: 238,
 			leftHandX: 166,
 			leftHandY: 258
 		},
 		{
 			eyeX: 235,
 			eyeY: 172,
-			rightHandX: 220,
-			rightHandY: 176,
+			rightHandX: 310,
+			rightHandY: 236,
 			leftHandX: 167,
 			leftHandY: 257
 		},
 		{
 			eyeX: 235,
 			eyeY: 173,
-			rightHandX: 220,
-			rightHandY: 176,
+			rightHandX: 309,
+			rightHandY: 238,
 			leftHandX: 172,
 			leftHandY: 258
 		},
 		{
 			eyeX: 230,
 			eyeY: 176,
-			rightHandX: 220,
-			rightHandY: 176,
+			rightHandX: 306,
+			rightHandY: 241,
 			leftHandX: 164,
 			leftHandY: 262
 		},
 		{
 			eyeX: 220,
 			eyeY: 174,
-			rightHandX: 220,
-			rightHandY: 176,
+			rightHandX: 302,
+			rightHandY: 239,
 			leftHandX: 157,
 			leftHandY: 258
 		},
 		{
 			eyeX: 219,
 			eyeY: 176,
-			rightHandX: 220,
-			rightHandY: 176,
+			rightHandX: 300,
+			rightHandY: 238,
 			leftHandX: 158,
 			leftHandY: 258
 		},
 		{
 			eyeX: 219,
 			eyeY: 177,
-			rightHandX: 220,
-			rightHandY: 176,
+			rightHandX: 299,
+			rightHandY: 240,
 			leftHandX: 159,
 			leftHandY: 258
 		}
