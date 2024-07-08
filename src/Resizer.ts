3e93a50a5a07b081b0f8092db4e0ad173a951da8
Slash <slash@slashware.net>
2024-07-07 21:41:05 -0500

    Center tablet

--- src/Resizer.ts
+++ src/Resizer.ts
@@ -15,6 +15,9 @@ class Resizer {
 		const heightScale = innerHeight / 892;
 		const scale = widthScale > heightScale ? heightScale : widthScale;
 		this.container.style.transform = `scale(${scale})`;
+		const tabletSuperContainer = document.getElementById('tabletSuperContainer');
+		tabletSuperContainer.style.width = `${1262 * scale}px`;
+		tabletSuperContainer.style.height = `${892 * scale}px`;
 		const tabletContainer = document.getElementById('tabletContainer');
 		pencil.updateScale(tabletContainer.getBoundingClientRect().width / tabletContainer.offsetWidth);
 	}
