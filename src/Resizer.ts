e95ce3b1162867a32cedcdd61274d3265f56b047
Slash <slash@slashware.net>
2024-07-08 11:12:50 -0500

    Add new tablet background

--- src/Resizer.ts
+++ src/Resizer.ts
@@ -13,12 +13,12 @@ class Resizer {
 		}
 		const referenceHeight = document.body.clientHeight;
 		const referenceWidth = document.body.clientWidth;
-		const widthScale = referenceWidth / 1262;
+		const widthScale = referenceWidth / 1399;
 		const heightScale = referenceHeight / 892;
 		const scale = widthScale > heightScale ? heightScale : widthScale;
 		this.container.style.transform = `scale(${scale})`;
 		const tabletSuperContainer = document.getElementById('tabletSuperContainer');
-		tabletSuperContainer.style.width = `${1262 * scale}px`;
+		tabletSuperContainer.style.width = `${1399 * scale}px`;
 		tabletSuperContainer.style.height = `${892 * scale}px`;
 		const tabletContainer = document.getElementById('tabletContainer');
 		pencil.updateScale(tabletContainer.getBoundingClientRect().width / tabletContainer.offsetWidth);
